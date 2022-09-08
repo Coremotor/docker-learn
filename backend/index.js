90519e5cf125538988e8d9b20eb72588d26a4d21
Andrey Demakov <coremotor@outlook.com>
2022-09-08 17:00:48 +0300

    cors

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,21 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const router = require("./src/router");
 
 const app = express();
 
-// const whitelist = ["http://194.67.74.121", "http://localhost"];
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-// };
-//
-// app.use(cors(corsOptions));
+const whitelist = ["http://194.67.74.121", "http://localhost"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router);
 
